eeb7aaf8fb171f2bbfc1fa27bddec6dba4af55b9
Bortnik <bortnikyarik@gmail.com>
2024-10-27 22:54:35 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication19/proj.cpp
@@ -0,0 +1,27 @@
+﻿#include "Student.h"
+#include "Programmer.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Human* people[2];
+
+    people[0] = new Student("Lincoln Burrows", 22, 3000.0, "Fox River");
+
+    people[1] = new Student("Michael Scoffield", 20, 2300.0, "Fox River");
+
+    for (int i = 0; i < 2; ++i)
+    {
+        people[i]->Output();
+        people[i]->Salary();
+        cout << endl;
+    }
+
+    for (int i = 0; i < 2; ++i)
+    {
+        delete people[i];
+    }
+
+    return 0;
+}
